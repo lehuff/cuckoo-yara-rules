90dca18a263c00a2e46990ac67d5558b02d3a450
lehuff <lanehuff@gmail.com>
2015-07-01 15:54:33 -0400

    Create cuckoo-yara-rules.py

---
+++ cuckoo-yara-rules.py
@@ -0,0 +1,65 @@
+'''
+The goal of this file is to grab all the files from the Yara-Rules github repo and then automatically add them to cuckoo, similar to the community.py tool for signatures.
+
+Ideally this will be run under /cuckoo/data/yara/
+'''
+
+import urllib2
+import glob
+import zipfile
+import os.path
+from macpath import dirname
+
+
+#Grab the current working directory for future use.
+cwd = os.getcwd()
+repo_path = cwd
+
+#Insert repos to clone here.
+repos = ['https://github.com/x64dbg/yarasigs/archive/master.zip',
+         'https://github.com/Yara-Rules/rules/archive/master.zip',
+         'https://github.com/hiddenillusion/yara-goodies/archive/master.zip',
+         'https://github.com/mjruffin/malware_signatures/archive/master.zip',
+         'https://github.com/schladt/public_yara_sigs/archive/master.zip'
+         ]
+etc = 1
+for each in repos:
+    try:
+        file = urllib2.urlopen(each)
+        data = file.read()
+        with open(("master" + str(etc) + ".zip"), "wb") as code:
+            code.write(data)
+        etc += 1
+    except Exception as e:
+        print(e)   
+         
+zips = []
+zipList = glob.glob('*.zip')
+zips.extend(zipList)
+
+for each in zips:
+    try:
+        zfile = zipfile.ZipFile(each)
+        for name in zfile.namelist():
+            (dirname, filename) = os.path.split(name)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+            zfile.extract(name, dirname)
+    except Exception as e:
+        print(e)
+
+yarFiles = []
+
+for root, dirs, files in os.walk(cwd):
+    for file in files:
+        if file.endswith('.yar'):
+            yarFiles.append(os.path.join(root, file))
+
+lFile = open('yara-rules-repo.yar', 'wb')
+
+for value in yarFiles:
+    try:
+        lFile.write('include ' + '"' + value + '"\n')
+    except Exception as e:
+        print(e)
+lFile.close()
